fa6610456a7d36982a73258c1c94374937c00f6f
Jeremy Odell <jeremyodell@gmail.com>
2024-01-07 07:33:57 -0600

    Initial commit

---
+++ src/main/java/com/odelldesigns/fishingapi/FishingApiApplication.java
@@ -0,0 +1,13 @@
+package com.odelldesigns.fishingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/odelldesigns/fishingapi/FishingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odelldesigns.fishingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
